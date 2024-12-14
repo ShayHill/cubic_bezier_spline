3cd4cecf338a5531ffa3795339fd7b4dc9d10f27
Shay <shay_public@hotmail.com>
2024-12-14 10:13:06 -0600

    fix: remove it.pairwise from tests
    it.pairwise fails CI tests for py 3.9 on Mac OS.

--- tests/test_svg_data.py
+++ tests/test_svg_data.py
@@ -5,12 +5,26 @@
 """
 
 import itertools as it
+from collections.abc import Iterable
+from typing import TypeVar
 
 from cubic_bezier_spline import (
     BezierSpline,
     new_closed_approximating_spline,
     new_open_approximating_spline,
 )
+
+_T = TypeVar("_T")
+
+
+def pairwise(iterable: Iterable[_T]) -> Iterable[tuple[_T, _T]]:
+    """Yield pairs of items from an iterable.
+
+    it.pairwise fails for python 3.9, but only on MacOS is CI tests.
+    """
+    a, b = it.tee(iterable)
+    _ = next(b, None)
+    return zip(a, b)
 
 
 class TestClosedC2Continuous:
@@ -26,11 +40,11 @@ class TestClosedC2Continuous:
         assert spline.svg_data == ("M 0,0C 1,0 2,0 2.5,0.5 3,1 3,2 2.5,2.5 2,3 1,3 0,3")
 
     def test_linear_closed(self):
-        curves = list(it.pairwise(((0, 0), (3, 0), (3, 3), (0, 3), (0, 0))))
+        curves = list(pairwise(((0, 0), (3, 0), (3, 3), (0, 3), (0, 0))))
         spline = BezierSpline(curves)
         assert spline.svg_data == "M 0,0H 3V 3H 0V 0Z"
 
     def test_linear_open(self):
-        curves = list(it.pairwise(((0, 0), (3, 0), (3, 3), (0, 3))))
+        curves = list(pairwise(((0, 0), (3, 0), (3, 3), (0, 3))))
         spline = BezierSpline(curves)
         assert spline.svg_data == "M 0,0H 3V 3H 0"
